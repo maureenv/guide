8c34e32e2a544667cf08d9292e32211e2e1a3ca4
maureenv <maureenvogel@outlook.com>
2019-04-26 21:44:52 -0400

    Updates

---
+++ precache-manifest.f755e36e466e0ec442c73c7173423425.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b9c7df8112f75433b80e57ccbdbac2",
+    "url": "/guide/index.html"
+  },
+  {
+    "revision": "3e6a24e893242f853dc1",
+    "url": "/guide/static/css/main.df406781.chunk.css"
+  },
+  {
+    "revision": "899114b5ded8549a8097",
+    "url": "/guide/static/js/2.36ba3b9c.chunk.js"
+  },
+  {
+    "revision": "3e6a24e893242f853dc1",
+    "url": "/guide/static/js/main.890fe516.chunk.js"
+  },
+  {
+    "revision": "86f4e0dcea104b34aa7d",
+    "url": "/guide/static/js/runtime~main.4cffee94.js"
+  },
+  {
+    "revision": "5c67920493d50f60ea27cef581a5219b",
+    "url": "/guide/static/media/ProximaNova-Black.5c679204.otf"
+  },
+  {
+    "revision": "72b6f2c9990bd3d37b8013a59ed78902",
+    "url": "/guide/static/media/ProximaNova-Bold.72b6f2c9.otf"
+  },
+  {
+    "revision": "0cd9a06ed7923acd0e769fa06775bef4",
+    "url": "/guide/static/media/ProximaNova-BoldIt.0cd9a06e.otf"
+  },
+  {
+    "revision": "5243e362912f7119ede836ab03f23ac7",
+    "url": "/guide/static/media/ProximaNova-Extrabold.5243e362.otf"
+  },
+  {
+    "revision": "ac51ad8a36d5bab6da11d454bcd06561",
+    "url": "/guide/static/media/ProximaNova-Light.ac51ad8a.otf"
+  },
+  {
+    "revision": "efc2820473a48f510ce341184e5c2daf",
+    "url": "/guide/static/media/ProximaNova-LightItalic.efc28204.otf"
+  },
+  {
+    "revision": "bf9f5d50c1b928ff21436517a1a95ad9",
+    "url": "/guide/static/media/ProximaNova-Regular.bf9f5d50.otf"
+  },
+  {
+    "revision": "c8708c7ae9f1ccfd893ee01efdf0ac51",
+    "url": "/guide/static/media/ProximaNova-RegularItalic.c8708c7a.otf"
+  },
+  {
+    "revision": "f14eee643541cf03a10f26c944cc29f5",
+    "url": "/guide/static/media/ProximaNova-Semibold.f14eee64.otf"
+  }
+]);
